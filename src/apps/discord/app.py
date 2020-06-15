4da0bdcd6ed5a8c61ef23b2a0e19dd92ab61c88e
Roy Moran <roy_moran1993@yahoo.com>
2020-06-15 01:23:18 -0500

    start discord client on background thread, enable server threads via … (#35)
    * start discord client on background thread, enable server threads via uwsgi
    
    * fix lint issues

--- src/apps/discord/app.py
+++ src/apps/discord/app.py
@@ -1,15 +1,13 @@
 """Entry point for discord flask app."""
-import os
 from flask import Flask
-from discord_client import DiscordClient
+from discord_client import init_discord_client_on_thread
 
 def create_app():
     """Startuo method to initialize flask and
        connect discord client.
     """
     flask_app = Flask(__name__)
-    client = DiscordClient()
-    client.run(os.environ['DISCORD_BOT_TOKEN'])
+    init_discord_client_on_thread()
     return flask_app
 
 app = create_app()
