4702954e8e8715fa01438d7c245a7e2c792f2c2d
Roy Moran <romoran1@outlook.com>
2020-06-29 10:10:35 -0500

    remove file contents

--- src/tests/test_app_events.py
+++ src/tests/test_app_events.py
@@ -3,37 +3,3 @@
 # TODO: Test scenario if channel is renamed
 # TODO: Test scenario if channel is removed
 # TODO: Test channel is created for each workspace
-"""Validate slack request"""
-import hashlib
-import hmac
-import os
-import slack
-import discord
-import asyncio
-import pytest
-from src.services.slack_workspace_service import SlackWorkspaceService
-from src.services.discord_workspace_service import DiscordWorkspaceService
-from src.shared_core.entry import Entry
-
-class TestAppEvents: # pylint: disable=too-few-public-methods
-    """RequestValidator contains a single method that
-       validates the slack request
-    """
-    pass
-    # @pytest.mark.skip
-    # def test_slack_channel_created_on_install(self):
-    #     # Arrange
-    #     workspace_services = {'slack': SlackWorkspaceService(), 'discord': DiscordWorkspaceService()}
-    #     slack_auth_token = 'xoxb-592647108848-1198713281185-WzWODm2xS2JPMb5wHMMO9wqe' # os.environ['SLACK_TEST_AUTH_TOKEN']
-    #     generated_channel_present = False
-    #     slack_client = slack.WebClient(slack_auth_token)
-    #     entry = Entry(workspace_services)
-    #     # Act
-    #     asyncio.run(entry.process_app_installed_event('slack', 'THEK136QY', 'Project Unicorn Test', slack_auth_token))
-    #     # Assert
-    #     response = slack_client.conversations_list()
-    #     channels = response['channels']
-    #     for channel in channels:
-    #         if channel['name'] == 'dev-questions':
-    #             generated_channel_present = True
-    #     assert generated_channel_present == True
