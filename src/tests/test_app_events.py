a435691d42931a40a8481359e4ebc1b0aad3f8c6
Roy Moran <romoran1@outlook.com>
2020-06-29 10:05:41 -0500

    comment out test method

--- src/tests/test_app_events.py
+++ src/tests/test_app_events.py
@@ -19,20 +19,21 @@ class TestAppEvents: # pylint: disable=too-few-public-methods
     """RequestValidator contains a single method that
        validates the slack request
     """
-    @pytest.mark.skip
-    def test_slack_channel_created_on_install(self):
-        # Arrange
-        workspace_services = {'slack': SlackWorkspaceService(), 'discord': DiscordWorkspaceService()}
-        slack_auth_token = 'xoxb-592647108848-1198713281185-WzWODm2xS2JPMb5wHMMO9wqe' # os.environ['SLACK_TEST_AUTH_TOKEN']
-        generated_channel_present = False
-        slack_client = slack.WebClient(slack_auth_token)
-        entry = Entry(workspace_services)
-        # Act
-        asyncio.run(entry.process_app_installed_event('slack', 'THEK136QY', 'Project Unicorn Test', slack_auth_token))
-        # Assert
-        response = slack_client.conversations_list()
-        channels = response['channels']
-        for channel in channels:
-            if channel['name'] == 'dev-questions':
-                generated_channel_present = True
-        assert generated_channel_present == True
+    pass
+    # @pytest.mark.skip
+    # def test_slack_channel_created_on_install(self):
+    #     # Arrange
+    #     workspace_services = {'slack': SlackWorkspaceService(), 'discord': DiscordWorkspaceService()}
+    #     slack_auth_token = 'xoxb-592647108848-1198713281185-WzWODm2xS2JPMb5wHMMO9wqe' # os.environ['SLACK_TEST_AUTH_TOKEN']
+    #     generated_channel_present = False
+    #     slack_client = slack.WebClient(slack_auth_token)
+    #     entry = Entry(workspace_services)
+    #     # Act
+    #     asyncio.run(entry.process_app_installed_event('slack', 'THEK136QY', 'Project Unicorn Test', slack_auth_token))
+    #     # Assert
+    #     response = slack_client.conversations_list()
+    #     channels = response['channels']
+    #     for channel in channels:
+    #         if channel['name'] == 'dev-questions':
+    #             generated_channel_present = True
+    #     assert generated_channel_present == True
