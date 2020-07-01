6cd62f35bee0dbcba406f47772ef6d1c4cc9d95e
Roy Moran <romoran1@outlook.com>
2020-07-01 06:23:37 -0500

    add channels:history to slack scope

--- src/apps/slack/app.py
+++ src/apps/slack/app.py
@@ -80,7 +80,7 @@ def info():
     return {
         'name': 'slack',
         'version': 'v0.0.13',
-        'install_url': f'https://slack.com/oauth/v2/authorize?client_id={client_id}&scope=channels:manage,channels:join,channels:read,chat:write,chat:write.customize,reactions:read,reactions:write,users:read&redirect_uri={redirect_uri}'
+        'install_url': f'https://slack.com/oauth/v2/authorize?client_id={client_id}&scope=channels:manage,channels:join,channels:read,chat:write,chat:write.customize,reactions:read,reactions:write,users:read,channels:history&redirect_uri={redirect_uri}'
     }
 
 def event_resolver(event_data):
