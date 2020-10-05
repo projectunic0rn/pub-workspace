81e2552eb37c4c94d5f646c2207d9041146c5807
Roy Moran <romoran1@outlook.com>
2020-10-05 01:09:12 -0500

    flip db echoing

--- src/persistence/db_config.py
+++ src/persistence/db_config.py
@@ -5,6 +5,6 @@ from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker
 
 connection_string = os.environ['WORKSPACES_CONNECTION_STRING']
-engine = create_engine(connection_string, echo=True, pool_pre_ping=True)
+engine = create_engine(connection_string, echo=False, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base(metadata=MetaData(), bind=engine)
