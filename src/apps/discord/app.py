c2afb9a6d65fb4d7fb02354bbd7c0810fed1146d
Roy Moran <romoran1@outlook.com>
2020-10-09 06:26:45 -0500

    add redirect uri and endpoint for discord

--- src/apps/discord/app.py
+++ src/apps/discord/app.py
@@ -5,6 +5,7 @@ from flask import Flask, Response
 from src.apps.const import APP_VERSION
 
 client_id = os.environ['DISCORD_CLIENT_ID']
+redirect_uri = os.environ["DISCORD_REDIRECT_URI"]
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -19,5 +20,9 @@ def info():
         'name': 'discord',
         'version': APP_VERSION,
         # Permissions Manage channels, View channels, Send messages, Add reactions
-        'installUrl': f'https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions=3152&scope=bot',
+        'installUrl': f'https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions=3152&scope=bot&redirect_uri={redirect_uri}',
     }
+
+@app.route("/finish_auth", methods=["POST"])
+def post_install():
+    return Response(status="200")
