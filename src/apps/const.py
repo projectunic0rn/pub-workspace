337b2d9ffbf253ec77c3302a5583b0e32bc4006f
Roy Moran <roy_moran1993@yahoo.com>
2020-10-23 07:21:51 -0500

    install alembic for managing db migrations (#85)
    * install alembic for managing db migrations
    
    * add pylint directive

--- src/apps/const.py
+++ src/apps/const.py
@@ -1,5 +1,5 @@
 """defines app version constant"""
-APP_VERSION = 'v0.0.34'
+APP_VERSION = 'v0.0.35'
 SLACK_WORKSPACE = 'slack'
 DISCORD_WORKSPACE = 'discord'
 DISCORD_API_ENDPOINT = 'https://discord.com/api/v6'
